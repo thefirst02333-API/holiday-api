99f03b8a20de794f6fa0c3d4dfb16bb12e3072cd
Meet <thefirst02333@gmail.com>
2026-06-28 20:43:26 +0530

    Day 11 - trigger redeploy

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.get("/holidays", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Holiday API is running on port ${PORT}`);
 });
